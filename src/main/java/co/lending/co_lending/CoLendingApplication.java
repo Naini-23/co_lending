edc67c78004ebbc83fa782cb362772b650e07da8
Anupam Mishra <203057@clix.local>
2024-05-13 21:56:20 +0530

    first commit

---
+++ src/main/java/co/lending/co_lending/CoLendingApplication.java
@@ -0,0 +1,13 @@
+package co.lending.co_lending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoLendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoLendingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/lending/co_lending/CoLendingApplicationTests.java
@@ -0,0 +1,13 @@
+package co.lending.co_lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoLendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
